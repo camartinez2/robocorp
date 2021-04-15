97f394cc7fda6e9b8130560716df1313cf9eabff
Cristian Adrián Martínez <camtech@192.168.225.82>
2021-04-14 20:03:36 -0500

    All

--- tasks2.py
+++ tasks2.py
@@ -6,10 +6,8 @@ user = "camtechadmin"
 password = "wzo@1rjtln$o192r"
 screenshot_filename = "output/screenshot.png"
 
-
 def open_the_website(url: str):
     browser.open_available_browser(url)
-
 
 def log_in(user: str, password: str):
     input_field = "id:ctl00_ContentPlaceHolder1_Login2_UserName"
@@ -17,13 +15,11 @@ def log_in(user: str, password: str):
     input_field = "id:ctl00_ContentPlaceHolder1_Login2_Password"
     browser.input_text(input_field, password)
     browser.press_keys(input_field, "ENTER")
-    input_field = "id:ctl00_ContentPlaceHolder2_AccordionPane1_content_LinkButFactura"
-    browser.press_keys(input_field, "ENTER")
-
+    #input_field = "id:ctl00_ContentPlaceHolder2_AccordionPane1_content_LinkButFactura"
+    #browser.press_keys(input_field, "ENTER")
 
 def store_screenshot(filename: str):
     browser.screenshot(filename=filename)
-
 
 # Define a main() function that calls the other functions in order:
 def main():
@@ -32,8 +28,7 @@ def main():
         log_in(user, password)
         store_screenshot(screenshot_filename)
     finally:
-        browser.close_all_browsers()
-
+       #browser.close_all_browsers()
 
 # Call the main() function, checking that we are running as a stand-alone script:
 if __name__ == "__main__":
