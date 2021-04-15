133f59ff91093931d7e7af122aa45660fa3b6443
Cristian Adrián Martínez <camtech@192.168.225.82>
2021-04-14 22:54:31 -0500

    All

--- tasks.py
+++ tasks.py
@@ -7,25 +7,26 @@ url_bill = "http://ebill.facturaenlinea.co/Contenido/Facturas/Edicion.aspx"
 user = "camtechadmin"
 password = "wzo@1rjtln$o192r"
 screenshot_filename = "output/screenshot.png"
+driver = webdriver.Chrome('/Users/camtech/Desktop/chromedriver')
 
 def open_the_website(url: str):
-    browser.open_available_browser(url)
-
+    driver.get(url)
 
 def log_in(user: str, password: str):
-    input_field = "id:ctl00_ContentPlaceHolder1_Login2_UserName"
-    browser.input_text(input_field, user)
-    input_field = "id:ctl00_ContentPlaceHolder1_Login2_Password"
-    browser.input_text(input_field, password)
-    browser.press_keys(input_field, "ENTER")
-    driver = webdriver.Chrome('/Users/camtech/Desktop/chromedriver')
-    login_form = driver.find_element_by_link_text('../../imagenes/MenuLateral/menu_cargarFactura.png')
-    actions = ActionChains(driver)
-    actions.click(login_form)
-    actions.perform()
+    element = driver.find_element_by_id('ctl00_ContentPlaceHolder1_Login2_UserName')
+    element.send_keys(user)
+    #input_field = "id:ctl00_ContentPlaceHolder1_Login2_UserName"
+    #browser.input_text(input_field, user)
+    #input_field = "id:ctl00_ContentPlaceHolder1_Login2_Password"
+    #browser.input_text(input_field, password)
+    #browser.press_keys(input_field, "ENTER")
+    #login_form = driver.find_element_by_link_text('../../imagenes/MenuLateral/menu_cargarFactura.png')
+    #actions = ActionChains(driver)
+    #actions.click(login_form)
+    #actions.perform()
 
 def store_screenshot(filename: str):
-    browser.screenshot(filename=filename)
+    driver.screenshot(filename=filename)
 
 
 # Define a main() function that calls the other functions in order:
@@ -35,7 +36,7 @@ def main():
         log_in(user, password)
         store_screenshot(screenshot_filename)
     finally:
-        browser.close_all_browsers()
+        driver.close_all_browsers()
 
 # Call the main() function, checking that we are running as a stand-alone script:
 if __name__ == "__main__":
