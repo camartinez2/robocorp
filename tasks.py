a565ac9af4c215a5d7f27f64e3ec5f432ce2a173
Cristian Adrián Martínez <camtech@192.168.225.82>
2021-04-14 20:23:20 -0500

    All

--- tasks.py
+++ tasks.py
@@ -2,6 +2,7 @@ from RPA.Browser.Selenium import Selenium
 
 browser = Selenium()
 url = "http://ebill.facturaenlinea.co/Login.aspx"
+url_bill = "http://ebill.facturaenlinea.co/Contenido/Facturas/Edicion.aspx"
 user = "camtechadmin"
 password = "wzo@1rjtln$o192r"
 screenshot_filename = "output/screenshot.png"
@@ -17,8 +18,7 @@ def log_in(user: str, password: str):
     input_field = "id:ctl00_ContentPlaceHolder1_Login2_Password"
     browser.input_text(input_field, password)
     browser.press_keys(input_field, "ENTER")
-    input_field = "id:ctl00_ContentPlaceHolder2_AccordionPane1_content_Image10"
-    browser.press_keys(input_field, "ENTER")
+    browser.open_available_browser(url_bill)
 
 def store_screenshot(filename: str):
     browser.screenshot(filename=filename)
