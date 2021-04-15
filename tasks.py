c1fa65ac4ea83a7c2afac29632fac5ee50bee38a
Cristian Adrián Martínez <camtech@192.168.225.95>
2021-04-15 18:18:12 -0500

    All

--- tasks.py
+++ tasks.py
@@ -24,7 +24,7 @@ def log_in(user: str, password: str):
     actions = ActionChains(driver)
     actions.click(link_bill)
     actions.perform()
-    seller = driver.find_element_by_id("ctl00_ContentPlaceHolder1_DDVendedores_I")
+    seller = driver.find_element_by_id("ctl00_ContentPlaceHolder1_DDVendedores_DDD_L_VI")
     seller.send_keys('Cristian Adrián Martínez')
 
 
