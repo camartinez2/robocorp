f7506a5f9a79b682c728c7304049085ec65a40b5
Cristian Adrián Martínez <camtech@192.168.225.82>
2021-04-14 20:15:46 -0500

    All

--- tasks.py
+++ tasks.py
@@ -19,6 +19,8 @@ def log_in(user: str, password: str):
     browser.press_keys(input_field, "ENTER")
     input_field = "id:ctl00_ContentPlaceHolder2_AccordionPane1_content_Image10"
     browser.press_keys(input_field, "ENTER")
+    img_bill = driver.find_element_by_id('ctl00_ContentPlaceHolder2_AccordionPane1_content_Image10')
+    img_bill.click()
 
 def store_screenshot(filename: str):
     browser.screenshot(filename=filename)
