1536a3866420e8f07d25820d09322ab5da945057
Cristian Adrián Martínez <camtech@192.168.225.95>
2021-04-15 22:25:21 -0500

    All

--- tasks.py
+++ tasks.py
@@ -22,8 +22,7 @@ def log_in(user: str, password: str):
     time.sleep(2)
     form_login.click()
     time.sleep(2)
-    link_bill = driver.find_element_by_id('create_invoice')
-    link_bill.click()
+    driver.get(urlbill)
 
 def store_screenshot(filename: str):
     time.sleep(10)
