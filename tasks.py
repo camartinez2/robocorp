45ff3586dca2efc288fec5eb79a51baeff4b471f
camartinez2 <78771806+camartinez2@users.noreply.github.com>
2021-05-24 04:12:32 -0500

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,14 +1,7 @@
-from RPA.Browser.Selenium import Selenium
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 import time
-import pymysql
 
-con = pymysql.connect(host='proyectouniandes.cxzzrkp6kclp.us-east-2.rds.amazonaws.com',
-        user='admin',
-        password='Temporal12345*',
-        db='nt_billed',
-        port=3306)
+import requests
+from selenium import webdriver
 
 url = "http://192.168.225.63:12121"
 urlbill = "http://192.168.225.63:12121/create_invoice.php"
@@ -18,8 +11,10 @@ password = "12345"
 screenshot_filename = "output/screenshot.png"
 driver = webdriver.Chrome('/Users/camtech/Desktop/chromedriver')
 
+
 def open_the_website(url: str):
     driver.get(url)
+
 
 def log_in(user: str, password: str):
     form_name = driver.find_element_by_id('email')
@@ -31,31 +26,29 @@ def log_in(user: str, password: str):
     form_login.click()
     time.sleep(2)
 
-def create_bill ():
-    try:
-        with con.cursor() as cur:
-            cur.execute('SELECT companyName, address, notes, productCode_1, productName_1, quantity_1, price_1, taxRate FROM nt_billed.billed where status = 0')
-            rows = cur.fetchall()
-            for row in rows:
-                driver.get(urlbill)
-                time.sleep(2)
-                companyName = row[0]
-                address = row[1]
-                notes = row[2]
-                productCode_1 = row[3]
-                productName_1 = row[4]
-                quantity_1 = row[5]
-                price_1 = row[6]
-                taxRate = row[7]
-                create_bill_client(companyName, address, notes)
-                create_bill_product(productCode_1, productName_1, quantity_1, price_1)
-                create_bill_taxes(taxRate)
-                time.sleep(2)
-                form_save = driver.find_element_by_id("invoice_btn")
-                form_save.click()
-                time.sleep(3)
-    finally:
-        con.close()
+
+def create_bill():
+    res = requests.get('http://18.223.235.79:7001/api/facturacion/rpa')
+    data = res.json()
+    for row in data:
+        driver.get(urlbill)
+        time.sleep(2)
+        companyName = row['companyName']
+        address = row['address']
+        notes = row['notes']
+        productCode_1 = row['productCode_1']
+        productName_1 = row['productName_1']
+        quantity_1 = row['quantity_1']
+        price_1 = row['price_1']
+        taxRate = row['taxRate']
+        create_bill_client(companyName, address, notes)
+        create_bill_product(productCode_1, productName_1, quantity_1, price_1)
+        create_bill_taxes(taxRate)
+        time.sleep(2)
+        form_save = driver.find_element_by_id("invoice_btn")
+        form_save.click()
+        time.sleep(3)
+
 
 def create_bill_client(companyName: str, address: str, notes: str):
     form_passwd = driver.find_element_by_id('companyName')
@@ -64,6 +57,7 @@ def create_bill_client(companyName: str, address: str, notes: str):
     form_passwd.send_keys(address)
     form_passwd = driver.find_element_by_id('notes')
     form_passwd.send_keys(notes)
+
 
 def create_bill_product(productCode_1: str, productName_1: str, quantity_1: str, price_1: str):
     form_passwd = driver.find_element_by_id('productCode_1')
@@ -75,23 +69,27 @@ def create_bill_product(productCode_1: str, productName_1: str, quantity_1: str,
     form_passwd = driver.find_element_by_id('price_1')
     form_passwd.send_keys(price_1)
 
+
 def create_bill_taxes(taxRate: str):
     form_passwd = driver.find_element_by_id('taxRate')
     form_passwd.send_keys(taxRate)
 
+
 def store_screenshot(filename: str):
     time.sleep(2)
-    #driver.save_screenshot(filename)
+    # driver.save_screenshot(filename)
+
 
 # Define a main() function that calls the other functions in order:
 def main():
     try:
         open_the_website(url)
         log_in(user, password)
-        create_bill ()
+        create_bill()
         store_screenshot(screenshot_filename)
     finally:
         driver.close()
+
 
 # Call the main() function, checking that we are running as a stand-alone script:
 if __name__ == "__main__":
